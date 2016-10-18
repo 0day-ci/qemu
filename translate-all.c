14f77526e06c158831490e937713528a1cd1ed4d
Pranith Kumar <bobby.prani@gmail.com>
2016-10-18 10:56:20 -0400

    translate-all: Use proper type
    gcc does not warn about the wrong type since it is a void pointer
    which can be cast to any type.
    
    Signed-off-by: Pranith Kumar <bobby.prani@gmail.com>

--- translate-all.c
+++ translate-all.c
@@ -412,7 +412,7 @@ static PageDesc *page_find_alloc(tb_page_addr_t index, int alloc)
 
     /* Level 2..N-1.  */
     for (i = V_L1_SHIFT / V_L2_BITS - 1; i > 0; i--) {
-        void **p = atomic_rcu_read(lp);
+        void *p = atomic_rcu_read(lp);
 
         if (p == NULL) {
             if (!alloc) {
