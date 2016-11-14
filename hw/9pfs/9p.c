80cbab6ab65e5a61438482ec9c9ae37cab9bca93
Li Qiang <liq3ea@gmail.com>
2016-11-14 07:32:56 -0500

    9pfs: add cleanup operation in FileOperations
    Currently, the backend of VirtFS doesn't have a cleanup
    function. This will lead resource leak issues if the backed
    driver allocates resources. This patch addresses this issue.
    
    Signed-off-by: Li Qiang <liq3ea@gmail.com>

--- hw/9pfs/9p.c
+++ hw/9pfs/9p.c
@@ -3532,6 +3532,9 @@ void v9fs_device_unrealize_common(V9fsState *s, Error **errp)
 {
     g_free(s->ctx.fs_root);
     g_free(s->tag);
+    if (s->ops->cleanup) {
+        s->ops->cleanup(&s->ctx);
+    }
 }
 
 typedef struct VirtfsCoResetData {
