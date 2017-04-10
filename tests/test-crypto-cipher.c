dae95d708aa4cf809c4393d3822e4e185abc99b3
Longpeng(Mike) <longpeng2@huawei.com>
2017-04-10 17:00:58 +0800

    tests: crypto: add cipher speed case
    Now we have afalg-backend and libiary-backend, it's necessary
    to add the speed test in test-crypto-cipher.
    
    We can use "./tests/test-crypto-cipher speed" to do the speed
    test.
    
    Signed-off-by: Longpeng(Mike) <longpeng2@huawei.com>

--- tests/test-crypto-cipher.c
+++ tests/test-crypto-cipher.c
@@ -789,6 +789,64 @@ static void test_cipher_short_plaintext(void)
     qcrypto_cipher_free(cipher);
 }
 
+static void test_cipher_speed(const void *opaque)
+{
+    QCryptoCipher *cipher;
+    Error *err = NULL;
+    double total = 0.0;
+    size_t chunk_size = (size_t)opaque;
+    uint8_t *key = NULL, *iv = NULL;
+    uint8_t *plaintext = NULL, *ciphertext = NULL;
+    size_t nkey = qcrypto_cipher_get_key_len(QCRYPTO_CIPHER_ALG_AES_128);
+    size_t niv = qcrypto_cipher_get_iv_len(QCRYPTO_CIPHER_ALG_AES_128,
+                                           QCRYPTO_CIPHER_MODE_CBC);
+
+    key = g_new0(uint8_t, nkey);
+    memset(key, g_test_rand_int(), nkey);
+
+    iv = g_new0(uint8_t, niv);
+    memset(iv, g_test_rand_int(), niv);
+
+    ciphertext = g_new0(uint8_t, chunk_size);
+
+    plaintext = g_new0(uint8_t, chunk_size);
+    memset(plaintext, g_test_rand_int(), chunk_size);
+
+    cipher = qcrypto_cipher_new(QCRYPTO_CIPHER_ALG_AES_128,
+                                QCRYPTO_CIPHER_MODE_CBC,
+                                key, nkey, &err);
+    g_assert(cipher != NULL);
+
+    g_assert(qcrypto_cipher_setiv(cipher,
+                                  iv, niv,
+                                  &err) == 0);
+
+    g_test_timer_start();
+    do {
+        g_assert(qcrypto_cipher_encrypt(cipher,
+                                        plaintext,
+                                        ciphertext,
+                                        chunk_size,
+                                        &err) == 0);
+        total += chunk_size;
+    } while (g_test_timer_elapsed() < 5.0);
+
+    total /= 1024 * 1024; /* to MB */
+
+    g_print("[drv:%s]", qcrypto_cipher_using_afalg_drv(cipher) ?
+            "afalg" : "libs");
+    g_print("Testing cbc(aes128): ");
+    g_print("Encrypting in chunks of %ld bytes: ", chunk_size);
+    g_print("done. %.2f MB in %.2f secs: ", total, g_test_timer_last());
+    g_print("%.2f MB/sec\t", total / g_test_timer_last());
+
+    qcrypto_cipher_free(cipher);
+    g_free(plaintext);
+    g_free(ciphertext);
+    g_free(iv);
+    g_free(key);
+}
+
 int main(int argc, char **argv)
 {
     size_t i;
@@ -797,17 +855,25 @@ int main(int argc, char **argv)
 
     g_assert(qcrypto_init(NULL) == 0);
 
-    for (i = 0; i < G_N_ELEMENTS(test_data); i++) {
-        if (qcrypto_cipher_supports(test_data[i].alg, test_data[i].mode)) {
-            g_test_add_data_func(test_data[i].path, &test_data[i], test_cipher);
+    if ((argc > 1) && !strcmp(argv[1], "speed")) {
+        for (i = 512; i <= (64 * 1204); i *= 2) {
+            g_test_add_data_func("/crypto/cipher/speed", (void *)i,
+                                 test_cipher_speed);
         }
+    } else {
+        for (i = 0; i < G_N_ELEMENTS(test_data); i++) {
+            if (qcrypto_cipher_supports(test_data[i].alg, test_data[i].mode)) {
+                g_test_add_data_func(test_data[i].path, &test_data[i],
+                                     test_cipher);
+            }
+        }
+
+        g_test_add_func("/crypto/cipher/null-iv",
+                        test_cipher_null_iv);
+
+        g_test_add_func("/crypto/cipher/short-plaintext",
+                        test_cipher_short_plaintext);
     }
-
-    g_test_add_func("/crypto/cipher/null-iv",
-                    test_cipher_null_iv);
-
-    g_test_add_func("/crypto/cipher/short-plaintext",
-                    test_cipher_short_plaintext);
 
     return g_test_run();
 }
